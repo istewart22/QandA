0ccb36262b75dc2e25e16e8cf3e05ee8df65ddd6
Iain Stewart <iainstewart08@gmail.com>
2022-05-07 10:30:20 +0100

    Add various links / further routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import { SearchPage } from './SearchPage';
 import { SignInPage } from './SignInPage';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
+import { QuestionPage } from './QuestionPage';
 import { NotFoundPage } from './NotFoundPage';
 import { fontFamily, fontSize, gray2 } from './Styles';
 
@@ -25,6 +26,7 @@ function App() {
           <Route path="search" element={<SearchPage />} />
           <Route path="ask" element={<AskPage />} />
           <Route path="signin" element={<SignInPage />} />
+          <Route path="questions/:questionId" element={<QuestionPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
