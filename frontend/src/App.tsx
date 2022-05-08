c75aef704bf3fc1066e418852dbb57bec6fcb6b8
Iain Stewart <iainstewart08@gmail.com>
2022-05-08 13:30:29 +0100

    Lazy load the ask page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AskPage } from './AskPage';
+import React, { lazy } from 'react';
 import { SearchPage } from './SearchPage';
 import { SignInPage } from './SignInPage';
 import { Header } from './Header';
@@ -9,6 +9,7 @@ import { HomePage } from './HomePage';
 import { QuestionPage } from './QuestionPage';
 import { NotFoundPage } from './NotFoundPage';
 import { fontFamily, fontSize, gray2 } from './Styles';
+const AskPage = lazy(() => import('./AskPage'));
 
 function App() {
   return (
@@ -24,7 +25,25 @@ function App() {
         <Routes>
           <Route path="" element={<HomePage />} />
           <Route path="search" element={<SearchPage />} />
-          <Route path="ask" element={<AskPage />} />
+          <Route
+            path="ask"
+            element={
+              <React.Suspense
+                fallback={
+                  <div
+                    css={css`
+                      margin-top: 100px;
+                      text-align: center;
+                    `}
+                  >
+                    Loading...
+                  </div>
+                }
+              >
+                <AskPage />
+              </React.Suspense>
+            }
+          />
           <Route path="signin" element={<SignInPage />} />
           <Route path="questions/:questionId" element={<QuestionPage />} />
           <Route path="*" element={<NotFoundPage />} />
