850f72c23ecfd82210109c1c4bbf7e08a462528a
Iain Stewart <iainstewart08@gmail.com>
2022-05-08 13:30:14 +0100

    Implement search page

--- frontend/src/QuestionData.ts
+++ frontend/src/QuestionData.ts
@@ -59,3 +59,13 @@ export const getQuestion = async (
   const results = questions.filter((q) => q.questionId === questionId);
   return results.length === 0 ? null : results[0];
 };
+
+export const searchQuestions = async (
+  criteria: string
+): Promise<QuestionData[]> => {
+  return questions.filter(
+    (q) =>
+      q.title.toLowerCase().indexOf(criteria.toLowerCase()) >= 0 ||
+      q.content.toLowerCase().indexOf(criteria.toLowerCase()) >= 0
+  );
+};
